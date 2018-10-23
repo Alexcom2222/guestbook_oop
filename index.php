45b6671ac98a781bec8962c003e582ec48554b24
alexcom2222 <alexcom2222@eclipso.eu>
2018-10-23 11:14:44 +0300

    OOP tutorial

--- index.php
+++ index.php
@@ -3,22 +3,23 @@ include_once('header.html');
 
 class MyClass
     {
+        // свойства и методы класса
+        public $prop1='свойство класса';
+
         public function __construct()
         {
             echo 'создан объект класса'.__CLASS__.'! <br />';
         }
 
-    // свойства и методы класса
-        public $prop1='свойство класса';
-
         public function setProperty($newval1)
-            {
-                $this->prop1=$newval1;
-            }
+        {
+            $this->prop1=$newval1;
+        }
+
         public function getProperty()
-            {
-                return $this->prop1.'<br>';
-            }
+        {
+            return $this->prop1.'<br>';
+        }
     }
 
 $obj_one= new MyClass();
